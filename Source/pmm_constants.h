c65666fd384b092a5d5e8645f98602094697e819
Kal Lapborisuth <kalsuda.lapborisuth@gmail.com>
2024-07-09 14:14:46 -0400

    convert submodule to cxx

---
+++ Source/pmm_constants.h
@@ -0,0 +1,11 @@
+
+/*! \file pmm_constants.c
+ \brief Global configuration constants.
+*/
+/*! > maximum line length of a matrix market file */
+#ifndef PMM_CONST_h
+#define PMM_CONST_h
+
+
+const int max_line_length=1026;
+#endif
